7950a0d95ad9e6f2c139f6a39bc08238e2c2598b
Milen Halachev <milenski88@gmail.com>
2016-03-04 00:05:26 +0200

    Initial Commit

---
+++ MHCoreDataKit/MHCoreDataKit.h
@@ -0,0 +1,19 @@
+//
+//  MHCoreDataKit.h
+//  MHCoreDataKit
+//
+//  Created by Milen Halachev on 3/4/16.
+//  Copyright © 2016 Milen Halachev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MHCoreDataKit.
+FOUNDATION_EXPORT double MHCoreDataKitVersionNumber;
+
+//! Project version string for MHCoreDataKit.
+FOUNDATION_EXPORT const unsigned char MHCoreDataKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MHCoreDataKit/PublicHeader.h>
+
+
